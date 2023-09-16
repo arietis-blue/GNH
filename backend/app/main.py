325c14d888d09e64c29ecaeef9b8e7df2b88d46c
arietis-blue <arietis20324@gmail.com>
2023-09-16 20:27:13 +0900

    mapcom

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,14 +10,16 @@ import os
 load_dotenv()
 firebase_cred_path = os.environ.get('FIREBASE_RELATIVE_PATH')
 
-# Firebase証明書のパスを確認
-if firebase_cred_path is None:
-    raise ValueError("The FIREBASE_RELATIVE_PATH environment variable is not set")
+# # Firebase証明書のパスを確認
+# if firebase_cred_path is None:
+#     raise ValueError("The FIREBASE_RELATIVE_PATH environment variable is not set")
 
-# Firebase証明書ファイルの存在を確認
-if not os.path.exists(firebase_cred_path):
-    raise ValueError(f"The specified Firebase credential file does not exist: {firebase_cred_path}")
+# # Firebase証明書ファイルの存在を確認
+# if not os.path.exists(firebase_cred_path):
+#     raise ValueError(f"The specified Firebase credential file does not exist: {firebase_cred_path}")
 
+cred = credentials.Certificate(firebase_cred_path)
+firebase_admin.initialize_app(cred)
 
 async def get_current_user(authorization: str = Header(...)):
     try:
